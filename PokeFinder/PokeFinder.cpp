37eea841e3239b6aad62700e732220ab8780b7c4
seamus.parkinson <seamus.parkinson@MININT-76KTRVB>
2026-03-16 13:28:41 +0000

    Base, json searching and one json to show me the way

---
+++ PokeFinder/PokeFinder.cpp
@@ -0,0 +1,31 @@
+#include "json.hpp"
+#include <fstream>
+#include <iostream>
+
+using namespace nlohmann;
+
+int main()
+{
+    json Kanto;
+    std::ifstream readStream("Region/Kanto.Goob");
+    Kanto = json::parse(readStream);
+    readStream.close();
+    for (auto element : Kanto["content"])
+    {
+        element["Name"].get<std::string>();
+        element["FileSource"].get<std::string>();
+    }
+
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to owpen this project again, go to File > Open > Project and select the .sln file
